6e0abcd462c51a210fe8ddec9f2cfd07a45e31de
Amrutha Lakshmi S <lakshmisamrutha@gmail.com>
2024-02-09 03:39:56 +0530

    models added

---
+++ EmployeeMgmt/EmployeeMgmt/Model/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeMgmt.Model
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string Role { get; set; }
+
+    }
+}
---
+++ EmployeeMgmt/EmployeeMgmt/Model/LeaveApplication.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeMgmt.Model
+{
+    public class LeaveApplication
+    {
+        public int ApplicationId { get; set; }
+        public int EmployeeId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string? Status { get; set; }
+        public string? Reason { get; set; }
+    }
+}
